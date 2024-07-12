c97fa16fe514eda71645d1f41124f946aff76011
manavnotop <manuagarwal1443@gmail.com>
2024-07-12 19:20:44 +0530

    add bulk route

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -107,4 +107,32 @@ router.put('/', authMiddleware, async function(req, res){
     })
 })
 
+router.get('/bulk', async function (req, res){
+    const filter = req.query.filter || "";
+
+    const users = await User.find({
+        $or : [
+            {
+                firstName: {
+                    $regex: filter
+                }
+            },
+            {
+                lastName: {
+                    $regex: filter
+                }
+            }
+        ]
+    });
+
+    res.status(200).json({
+        user: users.map(user => ({
+            username: user.username,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            _id: user._id
+        }))
+    })
+})
+
 module.exports = router;
