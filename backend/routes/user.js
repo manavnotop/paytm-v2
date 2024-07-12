f7595d74c85e96ec3eacf0c56a912bc370c22e97
manavnotop <manuagarwal1443@gmail.com>
2024-07-12 14:59:31 +0530

    add signin route

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -44,6 +44,45 @@ router.post('/signup', async function(req, res){
     const token = jwt.sign({
         userId
     }, JWT_SECRET);
+
+    res.json({
+        message: "User created successfully",
+        token: token
+    })
+})
+
+const signinBody = zod.object({
+    username: zod.string().email(),
+    password: zod.string()
+})
+
+router.post('/signin', async function(req, res){
+    const { success } = signinBody.safeParse(req.body);
+
+    if(!success){
+        return res.status(411).json({
+            message: "Incorrect inputs"
+        });
+    }
+
+    const user = await User.findOne({
+        username: req.body.username,
+        password: req.body.password
+    })
+
+    if(user){
+        const token = jwt.sign({
+            userId: user._id,
+        }, JWT_SECRET);
+
+        return res.status(200).json({
+            token: token
+        });
+    }
+
+    res.status(411).json({
+        message: "Error while loggin in"
+    })
 })
 
 module.exports = router;
