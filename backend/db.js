480ed71af4c6c725dfda8c08b67b6bd59a6558b4
manavnotop <manuagarwal1443@gmail.com>
2024-07-12 20:25:39 +0530

    add account db schema

--- backend/db.js
+++ backend/db.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const { number } = require('zod');
 require('dotenv').config();
 
 mongoose.connect(process.env.MONGO_URL);
@@ -22,8 +23,22 @@ const userSchema = mongoose.Schema({
     },
 });
 
-const User = mongoose.model("User", userSchema);
+const accoutSchema = mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    balance: {
+        type: number,
+        required: true
+    }
+})
+
+const User = mongoose.model('User', userSchema);
+const Account = mongoose.model('Account', accoutSchema);
 
 module.exports = {
     User,
+    Account
 };
