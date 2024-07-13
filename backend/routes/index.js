cbedaaf85108c28aee81df9ab91e0fae5e555901
manavnotop <manuagarwal1443@gmail.com>
2024-07-13 22:53:33 +0530

    add account router

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const userRouter = require('./user');
+const accoutRouter = require('./account');
 
 const router = express.Router();
 
 router.use("/user", userRouter);
+router.use("/account", accoutRouter);
 
 module.exports = router;
